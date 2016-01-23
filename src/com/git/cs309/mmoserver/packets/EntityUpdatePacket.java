5e8ed1f67da4880e6db76bf7c1d1575359103a95
Clownvin <123clownvin@gmail.com>
2016-01-22 21:26:03 -0600

    Changed EntityUpdatePacket

--- src/com/git/cs309/mmoserver/packets/EntityUpdatePacket.java
+++ src/com/git/cs309/mmoserver/packets/EntityUpdatePacket.java
@@ -3,15 +3,16 @@ package com.git.cs309.mmoserver.packets;
 import com.git.cs309.mmoserver.connection.AbstractConnection;
 
 public class EntityUpdatePacket extends Packet {
-	private final int positionX, positionY, destX, destY, id, anim, health;
+	private final int positionX, positionY, destX, destY, uniqueId, entityId, anim, health;
 	
-	public EntityUpdatePacket(AbstractConnection source, final int positionX, final int positionY, final int destX, final int destY, final int id, final int anim, final int health) {
+	public EntityUpdatePacket(AbstractConnection source, final int entityId, final int positionX, final int positionY, final int destX, final int destY, final int uniqueId, final int anim, final int health) {
 		super(source);
 		this.positionX = positionX;
 		this.positionY = positionY;
 		this.destX = destX;
 		this.destY = destY;
-		this.id = id;
+		this.uniqueId = uniqueId;
+		this.entityId = entityId;
 		this.anim = anim;
 		this.health = health;
 	}
@@ -23,9 +24,10 @@ public class EntityUpdatePacket extends Packet {
 		this.positionY = (bytes[index++] << 24) | (bytes[index++] << 16) | (bytes[index++] << 8) | bytes[index++];
 		this.destX = (bytes[index++] << 24) | (bytes[index++] << 16) | (bytes[index++] << 8) | bytes[index++];
 		this.destY = (bytes[index++] << 24) | (bytes[index++] << 16) | (bytes[index++] << 8) | bytes[index++];
-		this.id = (bytes[index++] << 24) | (bytes[index++] << 16) | (bytes[index++] << 8) | bytes[index++];
+		this.uniqueId = (bytes[index++] << 24) | (bytes[index++] << 16) | (bytes[index++] << 8) | bytes[index++];
 		this.anim = (bytes[index++] << 24) | (bytes[index++] << 16) | (bytes[index++] << 8) | bytes[index++];
 		this.health = (bytes[index++] << 24) | (bytes[index++] << 16) | (bytes[index++] << 8) | bytes[index++];
+		this.entityId = (bytes[index++] << 24) | (bytes[index++] << 16) | (bytes[index++] << 8) | bytes[index++];
 	}
 	
 	public int getPositionX() {
@@ -44,8 +46,12 @@ public class EntityUpdatePacket extends Packet {
 		return destY;
 	}
 	
-	public int getID() {
-		return id;
+	public int getEntityID() {
+		return entityId;
+	}
+	
+	public int getUniqueID() {
+		return uniqueId;
 	}
 	
 	public int getAnim() {
@@ -60,7 +66,7 @@ public class EntityUpdatePacket extends Packet {
 	public byte[] toBytes() {
 		byte[] bytes = new byte[29];
 		int index = 0;
-		int[] ints = {positionX, positionY, destX, destY, id, anim, health};
+		int[] ints = {positionX, positionY, destX, destY, uniqueId, anim, health, entityId};
 		bytes[index++] = getPacketType().getTypeByte();
 		for (int i = 0; i < ints.length; i++) {
 			bytes[index++] = (byte) (ints[i] >> 24);
